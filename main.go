60df717bfb014726dcd8d0e2e4a3bf2c0bda6652
Tomanta <bzednick@gmail.com>
2025-03-20 17:35:43 -0500

    Added input cleaning function and tests

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
     fmt.Println("Hello, World!")
 }
+
+func cleanInput(text string) []string {
+	result := []string{}
+	split_string := strings.Fields(text)
+	for i := range split_string {
+		result = append(result, strings.ToLower(split_string[i]))
+	}
+
+	return result
+}
+
---
+++ repl_test.go
@@ -0,0 +1,45 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	
+	cases := []struct {
+		input string
+		expected []string
+	}{
+		{
+			input: "   hello  world  ",
+			expected: []string{"hello","world"},
+		},
+		{
+			input: "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander","bulbasaur","pikachu"},
+		},
+		{
+			input: "heLLo",
+			expected: []string{"hello"},
+		},
+		// add more cases here
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		// Check the length of the actual slice against the expected slice
+		// if they don't match, use t.Errorf to print an error messsage
+		// and fail the test
+		if len(actual) != len(c.expected) {
+			t.Errorf("FAIL: Length does not match. Expected: %v, Actual: %v", len(c.expected), len(actual))
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+
+			if word != expectedWord {
+				t.Errorf("FAIL: word does not match. Expected: %v, Actual: %v", expectedWord, word)
+			}
+
+		}
+	}
+}
