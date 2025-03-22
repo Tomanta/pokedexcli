da647bf90cc51308e3fe926dd48725f4408ae44b
Tomanta <bzednick@gmail.com>
2025-03-22 11:29:32 -0500

    Added help and exit commands

--- repl.go
+++ repl.go
@@ -7,19 +7,55 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
 func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
-	
+
+	command_map := getCommands()
+
 	for {
 		fmt.Print("Pokedex > ")
-		scanner.Scan() 
+		scanner.Scan()
 
 		words := cleanInput(scanner.Text())
-		
+
 		if len(words) == 0 {
 			continue
 		}
-		fmt.Println("Your command was:", words[0])
+
+		cmd, exists := command_map[words[0]]
+		if exists {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+
 	}
 
 }
@@ -33,4 +69,3 @@ func cleanInput(text string) []string {
 
 	return result
 }
-
--- repl_test.go
+++ repl_test.go
@@ -3,21 +3,21 @@ package main
 import "testing"
 
 func TestCleanInput(t *testing.T) {
-	
+
 	cases := []struct {
-		input string
+		input    string
 		expected []string
 	}{
 		{
-			input: "   hello  world  ",
-			expected: []string{"hello","world"},
+			input:    "   hello  world  ",
+			expected: []string{"hello", "world"},
 		},
 		{
-			input: "Charmander Bulbasaur PIKACHU",
-			expected: []string{"charmander","bulbasaur","pikachu"},
+			input:    "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
 		},
 		{
-			input: "heLLo",
+			input:    "heLLo",
 			expected: []string{"hello"},
 		},
 		// add more cases here
